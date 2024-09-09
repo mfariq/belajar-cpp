085ef116112ef64b4c0efbc0cd2c5f2eb46de6f1
fariq <tonykriss2323@gmail.com>
2024-09-10 00:44:53 +0700

    pertama

---
+++ Hello world n comment/main.cpp
@@ -0,0 +1,8 @@
+// ini adalah single line comment yang dimana komentar menggunakan double slash dapat dipakai selama komentar hanya berjumlah 1 baris
+
+/* ini adalah multiple line comment yang dimana 
+komentar dapat berupa beberapa
+baris 
+*/
+
+#include <iostream> //baris ini merupakan deklarasi penggunaan folder input yang berupa istream dan output yang berupa ostream. 
