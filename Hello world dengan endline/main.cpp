6c83d43ec2e8ea7008beeb55de79e8562886ab2f
fariq <tonykriss2323@gmail.com>
2024-09-10 01:00:09 +0700

    third

---
+++ Hello world dengan endline/main.cpp
@@ -0,0 +1,15 @@
+// ini adalah single line comment yang dimana komentar menggunakan double slash dapat dipakai selama komentar hanya berjumlah 1 baris
+
+/* ini adalah multiple line comment yang dimana 
+komentar dapat berupa beberapa
+baris 
+*/
+
+#include <iostream> 
+using namespace std; 
+
+int main() {
+    cout << "Hello world!" << endl; //endl berfungsi sama seperti \n 
+    return 0;
+}
+
