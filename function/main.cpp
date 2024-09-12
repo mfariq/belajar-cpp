b51a82fef6b2bca984d69f0a48595480e603897f
fariq <tonykriss2323@gmail.com>
2024-09-12 23:41:54 +0700

    nineth

--- function/main.cpp
+++ function/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void jumlah(int a, int b) {
-    int a;
-    int b;
-    return a+b;
-};
+void jumlah(int a, int b);
 
 int main() {
+    int a1;
+    int a2;
+    cout << "Masukkan angka pertama : \n";
+    cin >> a1;
+    cout << "\nMasukkan angka kedua\n";
+    cin >> a2;
+    cout << "\nHasilnya adalah" << jumlah(a1,a2)
     
-}
+
+};
+
+ int jumlah(int a, int b){
+
+    return a+b;
+};
